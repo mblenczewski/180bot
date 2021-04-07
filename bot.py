1157aebb0decb7e5d7aa992b869467f90fc3591d
Mikołaj Lenczewski <mikolaj.lenczewski308@gmail.com>
2021-04-07 13:39:50 +0100

    Added reminder function

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ class BotClient(discord.Client):
         self.quotes = self.read_quotes()
 
         self.reload_quote_task = self.loop.create_task(self.bg_reload_quotes())
-                
+
 
     def read_last_reload_time(self):
         if not os.path.isfile(QUOTE_REFRESH_FILE):
@@ -108,6 +108,16 @@ class BotClient(discord.Client):
             quote_file.write(json.dumps(self.quotes, indent=4))
 
 
+    def get_quote_split(self, quote):
+        separators = ['- ', '– ', '— ']
+
+        for sep in separators:
+            if (sep_rindex := rindex(quote, sep)) is not None:
+                return sep_rindex
+
+        return None
+
+
     async def update_quotes(self, history, channel, limit=None, after=None):
         async for msg in channel.history(limit=limit, after=after, oldest_first=True):
             try:
@@ -115,17 +125,13 @@ class BotClient(discord.Client):
                 if msg.author.name == self.user.name:
                     continue
 
-                # ignore bot invocations
-                if msg.content.startswith(PREFIX):
-                    continue
-
                 # loose message format check
-                if (hyphen_rindex := rindex(msg.content, '- ')) is None:
+                if (quote_split := self.get_quote_split(msg.content)) is None:
                     continue
 
-                author = format_author_name(msg.content[hyphen_rindex+1:])
+                author = format_author_name(msg.content[quote_split+1:])
 
-                if ' me ' in f' {author.lower()} ':
+                if ' Me ' == f' {author} ':
                     author = format_author_name(msg.author.display_name)
 
                 if author not in self.authors:
@@ -185,6 +191,12 @@ class BotClient(discord.Client):
             print(err)
 
 
+    async def bg_reminder(self, author, minutes, message, set_date):
+        await asyncio.sleep(minutes * 60)
+        # await author.send(f'(@{set_date}) Reminder: {message}')
+        await author.send(f'Reminder: {message}')
+
+
     async def on_message(self, message):
         if message.author == self.user:
             return
@@ -198,10 +210,10 @@ class BotClient(discord.Client):
             for command, handler in COMMANDS.items():
                 if key == command:
                     try:
-                        await handler(self, message.channel, params)
+                        await handler(self, message.author, message.channel, params)
                     except Exception as err:
                         print(f'Error duing {command} handler (params: {params}):\n{err}')
-                        error = err_embed('Error', 'Invalid usage! Please see .help or .usage')
+                        error = err_embed('Error', f'{err}\nInvalid usage! Please see .help or .usage')
                         await message.channel.send(embed=error)
                     break
             else:
@@ -209,7 +221,7 @@ class BotClient(discord.Client):
 
 
     @command(keys=['help', 'usage'], params=[], usage='Displays usage information')
-    async def usage(self, channel, params):
+    async def usage(self, author, channel, params):
         response = 'Available Commands:\n'
         for key in COMMANDS:
             description, parameters = DESCRIPTIONS[key], PARAMETERS[key]
@@ -222,12 +234,12 @@ class BotClient(discord.Client):
 
 
     @command(keys=['ping'], params=[], usage='Returns with a pong')
-    async def ping(self, channel, params):
+    async def ping(self, author, channel, params):
        await channel.send('pong')
 
 
     @command(keys=['totals', 'tally'], params=[], usage='Quote totals per person')
-    async def totals(self, channel, params):
+    async def totals(self, author, channel, params):
         response, total_quotes= 'Quotes per capita:\n', 0
 
         for author, info in self.authors.items():
@@ -240,14 +252,14 @@ class BotClient(discord.Client):
 
 
     @command(keys=['quote'], params=[], usage='Gets a random quote')
-    async def quote(self, channel, params):
+    async def quote(self, author, channel, params):
         index = random.randint(0, len(self.quotes)-1)
 
         await channel.send(embed=embed(f'Quote #{index}', self.quotes[index]['quote']))
 
 
     @command(keys=['uquote'], params=['<username:str>'], usage='Gets a random quote from the given user')
-    async def user_quote(self, channel, params):
+    async def user_quote(self, author, channel, params):
         author = format_author_name(' '.join(params))
         author_quotes = self.authors[author]['quotes']
         index = random.randint(0, len(author_quotes)-1) 
@@ -255,31 +267,82 @@ class BotClient(discord.Client):
         await channel.send(embed=embed(f'{author}\'s Quote #{index}', author_quotes[index]))
 
 
-    @command(keys=['uhistory'], params=['<username:str'], usage='Gets the given users quote history')
-    async def user_history(self, channel, params):
+    @command(keys=['uiquote'], params=['<username:str>', '<index:int>'], usage='')
+    async def user_indexed_quote(self, author, channel, params):
+        author = format_author_name(' '.join(params[:-1]))
+        author_quotes = self.authors[author]['quotes']
+        index = int(params[-1])
+
+        await channel.send(embed=embed(f'{author}\'s Quote #{index}', author_quotes[index]))
+
+
+    @command(keys=['uhistory'], params=['<username:str>'], usage='Gets the given users quote history')
+    async def user_history(self, author, channel, params):
         author = format_author_name(' '.join(params))
         author_quotes = self.authors[author]['quotes']
 
         response, page = '', 1
         for idx, quote in enumerate(author_quotes):
-                if 2048 <= len(response) + len(quote):
-                    await channel.send(embed=embed(f'{author}\'s History ({page})', response))
-                    response = ''
-                    page += 1
+            formatted_quote = f'#{idx}: {quote}\n'
 
-                response += f'#{idx}: {quote}\n'
+            if 2048 <= len(response) + len(formatted_quote):
+                await channel.send(embed=embed(f'{author}\'s History ({page})', response))
+                response = ''
+                page += 1
+
+            response += formatted_quote
 
         await channel.send(embed=embed(f'{author}\'s History ({page})', response))
 
 
     @command(keys=['iquote'], params=['<index:int>'], usage='Gets the quote with the given index')
-    async def index_quote(self, channel, params):
+    async def index_quote(self, author, channel, params):
         index = int(params[0])
 
         if index < 0 or len(self.quotes) <= index:
             raise ValueError(f'given index {index} is out of bounds')
 
         await channel.send(embed=embed(f'Quote #{index}', self.quotes[index]['quote']))
+
+
+    @command(keys=['test'], params=['<quote:str>'], usage='Checks whether a quote would be accepted')
+    async def test_quote(self, author, channel, params):
+        quote = ' '.join(params)
+        response = ''
+
+        if (quote_split := self.get_quote_split(quote)) is None:
+            response = 'Quote not accepted!'
+        else:
+            response = f'Quote accepted! Author separator index: {quote_split}'
+
+        await channel.send(embed=embed(f'Quote Test: \'{quote}\'', response))
+
+
+    @command(keys=['remind'], params=['<time:int> <scale:hr|min> - <message:str>'], usage='Reminds you about a message after a given time')
+    async def set_reminder(self, author, channel, params):
+        reminder_mins, reminder_scale = int(params[0]), params[1]
+
+        if reminder_scale != 'hr' and reminder_scale != 'min':
+            await channel.send(embed=err_embed('Invalid timescale', 'Please use hr (hours) or min (minutes)'))
+            return
+
+        if reminder_scale == 'hr':
+            reminder_mins *= 60
+
+        reminder_message = ' '.join(params[2:])
+        if '- ' != reminder_message[0:2]:
+            await channel.send(embed=err_embed('Invalid separator', 'Please use the \'-\' separator between the expiry and the message'))
+            return
+
+        now = datetime.datetime.now()
+        reminder_expiry = now + datetime.timedelta(minutes=reminder_mins)
+
+        reminder_task = self.bg_reminder(author, reminder_mins, reminder_message[2:], now.isoformat(' ', 'seconds'))
+        self.loop.create_task(reminder_task)
+
+        expiry = reminder_expiry.isoformat(' ', 'seconds')
+
+        await channel.send(embed=embed(f'Set reminder', f'Set the reminder for {expiry} UTC'))
 
 
 def main():
